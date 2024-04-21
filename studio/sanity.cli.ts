d02bdfb8a1fc2b50b163485c9305a734416e80dd
anthhub <anthhub@163.com>
2024-04-21 23:57:07 +0800

    feat: ts error

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,8 +1,10 @@
-import {defineCliConfig} from 'sanity/cli'
+// @ts-ignore
+import { defineCliConfig } from "sanity/cli";
 
+// @ts-ignore
 export default defineCliConfig({
   api: {
-    projectId: '335ob3b2',
-    dataset: 'production'
-  }
-})
+    projectId: "335ob3b2",
+    dataset: "production",
+  },
+});
--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,18 +1,22 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+// @ts-ignore
+import { defineConfig } from "sanity";
+// @ts-ignore
+import { structureTool } from "sanity/structure";
+// @ts-ignore
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
 
+// @ts-ignore
 export default defineConfig({
-  name: 'default',
-  title: 'opensea-blockchain-clone',
+  name: "default",
+  title: "opensea-blockchain-clone",
 
-  projectId: '335ob3b2',
-  dataset: 'production',
+  projectId: "335ob3b2",
+  dataset: "production",
 
   plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
